e1e56fd2896adb1ae535f778a25dfa07ee9e4173
yululuya <whaleyl@163.com>
2021-02-22 16:25:14 +0800

    no message

--- HJJVue/src/views/Index.vue
+++ HJJVue/src/views/Index.vue
@@ -1673,26 +1673,6 @@ export default {
       );
       a.dispatchEvent(e);
     },
-    //生成guid
-    guid() {
-      function S4() {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-      }
-      return (
-        S4() +
-        S4() +
-        "-" +
-        S4() +
-        "-" +
-        S4() +
-        "-" +
-        S4() +
-        "-" +
-        S4() +
-        S4() +
-        S4()
-      );
-    },
   },
 };
 </script>
